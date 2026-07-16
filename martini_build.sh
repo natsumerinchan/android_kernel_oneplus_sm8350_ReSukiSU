c4f54446fee24829c3013f05c5cd0e994ab7665b
Celica Sylphil <kamikitakomaric@protonmail.com>
2026-07-16 19:23:48 +0800

    Add ReSukiSU

---
+++ martini_build.sh
@@ -0,0 +1,88 @@
+#! /bin/bash
+
+set -x
+
+setup_export() {
+    export KERNEL_PATH=$PWD
+    export KERNEL_DEFCONFIG=martini_defconfig
+    export KERNEL_FILE=Image
+    export BUILD_EXTRA_COMMAND='LLVM=1'
+    export USE_KERNELSU=true
+    export LTO_DISABLE=false
+}
+
+setup_build_kernel_env() {
+    cd $KERNEL_PATH
+    BUILD_TIME=$(TZ=Asia/Shanghai date "+%Y%m%d%H%M")
+    if test ! -e ~/toolchains/clang-aosp/env_is_setup; then
+      sudo pacman -S zstd tar wget curl base-devel cpio --noconfirm
+      wget -O ./lib32-ncurses.pkg.tar.zst https://archlinux.org/packages/multilib/x86_64/lib32-ncurses/download/
+      wget -O ./lib32-readline.pkg.tar.zst https://archlinux.org/packages/multilib/x86_64/lib32-readline/download/
+      wget -O ./lib32-zlib.pkg.tar.zst https://archlinux.org/packages/multilib/x86_64/lib32-zlib/download/
+      sudo pacman -U ./lib32-ncurses.pkg.tar.zst ./lib32-readline.pkg.tar.zst ./lib32-zlib.pkg.tar.zst --noconfirm
+      rm ./*.zst
+      yay -S lineageos-devel python2-bin --noconfirm
+    fi
+    git submodule init
+    git submodule update --remote --recursive
+}
+
+aosp_clang() {
+    if test ! -e ~/toolchains/clang-aosp/env_is_setup; then
+        rm -rf ~/toolchains/clang-aosp && mkdir -p ~/toolchains/clang-aosp && cd ~/toolchains/clang-aosp
+        wget https://github.com/SA9990/Toolchain/releases/download/clang-r563880c/clang-r563880c.tar.gz
+        tar -C ~/toolchains/clang-aosp/ -zxvf clang-r563880c.tar.gz
+        touch ~/toolchains/clang-aosp/env_is_setup
+    fi
+}
+
+setup_kernelsu() {
+    cd $KERNEL_PATH
+    curl -LSs "https://raw.githubusercontent.com/ReSukiSU/ReSukiSU/main/kernel/setup.sh" | bash -s main
+    if test "$LTO_DISABLE" == "true"; then
+        sed -i 's/CONFIG_LTO=y/CONFIG_LTO=n/' $KERNEL_PATH/arch/arm64/configs/$KERNEL_DEFCONFIG
+        sed -i 's/CONFIG_LTO_CLANG=y/CONFIG_LTO_CLANG=n/' $KERNEL_PATH/arch/arm64/configs/$KERNEL_DEFCONFIG
+        sed -i 's/CONFIG_THINLTO=y/CONFIG_THINLTO=n/' $KERNEL_PATH/arch/arm64/configs/$KERNEL_DEFCONFIG
+        echo "CONFIG_LTO_NONE=y" >> $KERNEL_PATH/arch/arm64/configs/$KERNEL_DEFCONFIG
+    fi
+}
+
+build_kernel() {
+    cd $KERNEL_PATH
+    export PATH=~/toolchains/clang-aosp/bin:$PATH
+    make -j$(nproc --all) O=out ARCH=arm64 CLANG_TRIPLE=aarch64-linux-gnu- CC="ccache clang" CXX="ccache clang++" $BUILD_EXTRA_COMMAND $KERNEL_DEFCONFIG
+    time make -j$(nproc --all) O=out ARCH=arm64 CLANG_TRIPLE=aarch64-linux-gnu- CC="ccache clang" CXX="ccache clang++" $BUILD_EXTRA_COMMAND
+}
+
+make_anykernel3() {
+    cd $KERNEL_PATH
+    test -d $KERNEL_PATH/AnyKernel3 || git clone https://github.com/osm0sis/AnyKernel3
+    if test -e $KERNEL_PATH/out/arch/arm64/boot/$KERNEL_FILE && test -d $KERNEL_PATH/AnyKernel3; then
+        sed -i 's/do.devicecheck=1/do.devicecheck=0/g' AnyKernel3/anykernel.sh
+        sed -i 's!BLOCK=/dev/block/platform/omap/omap_hsmmc.0/by-name/boot;!BLOCK=auto;!g' AnyKernel3/anykernel.sh
+        sed -i 's/IS_SLOT_DEVICE=0;/IS_SLOT_DEVICE=auto;/g' AnyKernel3/anykernel.sh
+        cp $KERNEL_PATH/out/arch/arm64/boot/$KERNEL_FILE $KERNEL_PATH/AnyKernel3/$KERNEL_FILE
+        rm -rf AnyKernel3/.git* AnyKernel3/README.md
+        cd $KERNEL_PATH/AnyKernel3
+        zip -r Anykernel3.zip *
+        mv $KERNEL_PATH/AnyKernel3/Anykernel3.zip $KERNEL_PATH/out/arch/arm64/boot
+        rm -r $KERNEL_PATH/AnyKernel3/$KERNEL_FILE
+        cd $KERNEL_PATH
+        echo [INFO] Products are put in $KERNEL_PATH/out/arch/arm64/boot
+        echo [INFO] Done.
+    fi
+}
+
+setup_export
+setup_build_kernel_env
+aosp_clang
+
+if [ "${1-}" == "clean" ] || [ "${2-}" == "clean" ]; then
+    test -d ~/.ccache && rm -rf ~/.ccache
+    test -d ~/.cache/ccache && rm -rf ~/.cache/ccache
+    test -d "$KERNEL_PATH/out" && rm -rf "$KERNEL_PATH/out"
+fi
+
+setup_kernelsu
+build_kernel
+make_anykernel3
